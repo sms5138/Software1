f94cfb0a42020b9b737a58e46013b07f16887f24
sms5138 <seanm.sawyer@gmail.com>
2021-10-05 14:26:56 -0500

    update gui to launch from fxml

--- Software1.java
+++ Software1.java
@@ -1,40 +1,22 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.control.TextField;
+// import javafx.scene.layout.Pane;
+// import javafx.scene.control.TextField;
 
 public class Software1 extends Application {
-   @Override
-   public void start(Stage applicationStage) {
-      int hourlyWage;
-      int yearlySalary;
-      Scene scene = null;                  // Scene contains all content
-      Pane pane = null;                    // Positions components within scene
-      TextField outputField = null;        // Displays output salary
-      
-      pane = new Pane();                   // Create an empty pane     
-      scene = new Scene(pane);             // Create a scene containing the pane
-            
-      // Calculate yearly salary
-      hourlyWage = 20;
-      yearlySalary = hourlyWage * 40 * 50;
-              
-      // Create text field and display program output using the text field
-      outputField = new TextField();
-      outputField.setText("An hourly wage of $" + hourlyWage + "/hr " +
-                          "yields $" + yearlySalary + "/yr.");
-      outputField.setEditable(false);      // Prevent user from editing text
-      outputField.setPrefColumnCount(22);  
-      
-      pane.getChildren().add(outputField); // Add text field to pane
-      
-      applicationStage.setScene(scene);    // Set window's scene  
-      applicationStage.setTitle("Salary"); // Set window's title
-      applicationStage.show();             // Display window
-   }
-   
-   public static void main(String [] args) {
-      launch(args); // Launch application
-   }
+      public static void main(String[] args){
+         launch(args);
+      }
+
+      @Override
+      public void start(Stage primaryStage) throws Exception{
+         Parent root = FXMLLoader.load(getClass().getResource("main_form.fxml"));
+         primaryStage.setTitle("main page");
+         primaryStage.setScene(new Scene(root, 640, 400));
+         primaryStage.show();
+
+      }
 }
