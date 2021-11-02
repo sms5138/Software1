0d432c7c7738f85341a5e9c38fe169255dcd6b8c
sms5138 <seanm.sawyer@gmail.com>
2021-11-02 09:58:57 -0500

    creating Product models

--- Software1/Model/part_outsource.java
+++ Software1/Model/part_outsource.java
@@ -2,7 +2,7 @@ package Software1.Model;
 
 public class part_outsource extends Part{
 
-    // this is just for example currently and is not being used.
+    // this overrides the name from the Part model being used.
     private String name;
 
     public part_outsource(int id, String name, double price, int stock, int min, int max, String outsource_name) {
--- Software1/Model/product_inventory.java
+++ Software1/Model/product_inventory.java
@@ -1,5 +1,16 @@
 package Software1.Model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class product_inventory {
-    
+    private static ObservableList<Part> the_inventory = FXCollections.observableArrayList();
+
+    public static void addInventoryItems(Part parts){
+        the_inventory.add(parts);
+    }
+
+    public static ObservableList<Part> getInventoryItems(){
+        return the_inventory;
+    }
 }
