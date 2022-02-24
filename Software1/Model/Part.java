6a88ba31eaf2b8b9cb7f3c50d7cea5fc577287d7
sms5138 <seanm.sawyer@gmail.com>
2022-02-24 12:07:38 -0600

    correcting for JavaDocs

--- Software1/Model/Part.java
+++ Software1/Model/Part.java
@@ -1,12 +1,12 @@
 package Software1.Model;
 
-/*
+/**
  * Supplied class Part.java 
  */
 
-/*
+/**
  *
- * @author Sean Sawyer
+ * @author Place Your Name Here
  */
 public abstract class Part {
     private int id;
@@ -24,84 +24,84 @@ public abstract class Part {
         this.max = max;
     }
 
-    /*  
+    /**
      * @return the id
-     */ 
+     */
     public int getId() {
         return id;
     }
 
-    /*
+    /**
      * @param id the id to set
      */
     public void setId(int id) {
         this.id = id;
     }
 
-    /*
+    /**
      * @return the name
      */
     public String getName() {
         return name;
     }
 
-    /*
+    /**
      * @param name the name to set
      */
     public void setName(String name) {
         this.name = name;
     }
 
-    /*
+    /**
      * @return the price
      */
     public double getPrice() {
         return price;
     }
 
-    /*
+    /**
      * @param price the price to set
      */
     public void setPrice(double price) {
         this.price = price;
     }
     
-    /*
+    /**
      * @return the stock
      */
     public int getStock() {
         return stock;
     }
 
-    /*
+    /**
      * @param stock the stock to set
      */
     public void setStock(int stock) {
         this.stock = stock;
     }
 
-    /*
+    /**
      * @return the min
      */
     public int getMin() {
         return min;
     }
 
-    /*
+    /**
      * @param min the min to set
      */
     public void setMin(int min) {
         this.min = min;
     }
 
-    /*
+    /**
      * @return the max
      */
     public int getMax() {
         return max;
     }
 
-    /*
+    /**
      * @param max the max to set
      */
     public void setMax(int max) {
